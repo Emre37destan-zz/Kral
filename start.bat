ba78cb3497dd37861734b9eec194ba07809acaaf
prajwal0909 <88018651+prajwal0909@users.noreply.github.com>
2021-07-27 10:32:44 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,7 +3,7 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2019 By MBAH GADGET" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user administrator Kadim1998 /add >nul
+net user administrator prajwal0909 /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 net user installer /delete
@@ -15,11 +15,11 @@ ICACLS C:\Windows\installer /grant administrator:F >nul
 echo Success!
 echo You RDP run successfully
 echo If you need help don't hesitate to talk to me
-echo My Facebook https://www.facebook.com/Kadim1998.Iraqi
+echo My Facebook https://www.facebook.com/prajwal0909
 echo INFO RDP
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
 echo Username: administrator
-echo Password: Kadim1998
+echo Password: prajwal0909
 echo You can login now.
 ping -n 10 127.0.0.1 >nul
