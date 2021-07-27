e410816ddbf1547620048db687b828bcd30d0629
prajwal0909 <88018651+prajwal0909@users.noreply.github.com>
2021-07-27 11:09:18 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -20,6 +20,6 @@ echo INFO RDP
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
 echo Username: administrator
-echo Password: prajwal0909
+echo Password: kadim1998
 echo You can login now.
 ping -n 10 127.0.0.1 >nul
