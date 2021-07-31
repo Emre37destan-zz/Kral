3fa3f697dcdc4659f025148f1e0fb0e125048924
Kral <34852206+Emre37destan@users.noreply.github.com>
2021-07-31 04:30:06 +0300

    Kral
    Kral

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Server 2019 By MBAH GADGET" > out.txt 2>&1
+net config server /srvcomment:"Win10 Kral" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
 net user administrator Kastamonu37 /add >nul
@@ -12,14 +12,14 @@ sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
-echo Success!
-echo You RDP run successfully
-echo If you need help don't hesitate to talk to me
-echo My Facebook https://www.facebook.com/prajwal0909
-echo INFO RDP
+echoBaşarı!
+echo RDP'yi başarıyla çalıştırdınız
+echo Yardıma ihtiyacın olursa benimle konuşmaktan çekinme
+echo Benim E-Posta; email:destan3673@hotmail.com
+echo BİLGİ RDP
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
-echo Username: administrator
-echo Password: Kastamonu37
-echo You can login now.
+echo Kullanıcı adı: administrator
+echo Parola: Kastamonu37
+echo Şimdi giriş yapabilirsiniz.
 ping -n 10 127.0.0.1 >nul
