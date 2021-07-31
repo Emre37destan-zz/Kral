16309490b122ba37c56fafda2811732c7c1a558d
Kral <34852206+Emre37destan@users.noreply.github.com>
2021-07-31 04:33:09 +0300

    Kral
    Kral

--- start.bat
+++ start.bat
@@ -12,11 +12,11 @@ sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
-echoBaşarı!
+echo Başarı!
 echo RDP'yi başarıyla çalıştırdınız
 echo Yardıma ihtiyacın olursa benimle konuşmaktan çekinme
 echo Benim E-Posta; email:destan3673@hotmail.com
-echo BİLGİ RDP
+echo INFO RDP
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
 echo Kullanıcı adı: administrator
